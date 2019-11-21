a6dd6c25e79455caafd0950a9199a445a9470797
Sergio Hideki <sergiohidekiono@gmail.com>
2019-11-21 16:39:49 -0300

    Request the service to the component and define the snapshot of the route

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Restaurant } from './../restaurants/restaurant/restaurant.model';
+import { RestaurantService } from './../restaurants/restaurants.service';
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'mt-restaurant-detail',
   templateUrl: './restaurant-detail.component.html'
 })
 export class RestaurantDetailComponent implements OnInit {
 
-  constructor() { }
+  restaurant: Restaurant
+
+  constructor(
+    private restaurantsService: RestaurantService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.restaurantsService.restaurantById(this.route.snapshot.params['id'])
+      .subscribe((retornoAPI) => {
+        this.restaurant = retornoAPI
+      })
   }
 
 }
