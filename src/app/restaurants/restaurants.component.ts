7a7ae71d0ca93600bef0bbd92712fb8d803236e5
Sergio Hideki <sergiohidekiono@gmail.com>
2019-12-20 00:42:32 -0300

    Create searchbar and animate

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -2,12 +2,29 @@ import { Component, OnInit } from '@angular/core';
 
 import { Restaurant } from './restaurant/restaurant.model';
 import { RestaurantService } from './restaurants.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'mt-restaurants',
   templateUrl: './restaurants.component.html',
+  animations: [
+    trigger('toggleSearch', [
+      state('hidden', style({
+        opacity: 0,
+        'max-height': '0px'
+      })),
+      state('visible', style({
+        opacity: 1,
+        'max-height': '70px',
+        'margin-top': '20px'
+      })),
+      transition('* => *', animate('250ms 0s ease-in-out'))
+    ])
+  ]
 })
 export class RestaurantsComponent implements OnInit {
+
+  searchBarState = 'hidden'
 
   restaurants: Restaurant[] = []
 
@@ -18,6 +35,10 @@ export class RestaurantsComponent implements OnInit {
       .subscribe((retornoAPI) => {
         this.restaurants = retornoAPI;
       })
+  }
+
+  toggleSearch() {
+    this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden'
   }
 
 }
